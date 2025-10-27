0e77486d981b1765effb280ce14d41c29e55ad68
tuandz9999 <tuantvph34792@fpt.edu.vn>
2025-10-27 15:59:00 +0700

    fix update post

--- resources/views/admin/notifications/create.blade.php
+++ resources/views/admin/notifications/create.blade.php
@@ -52,7 +52,6 @@
 
 @push('scripts')
     <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Select2 -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
